9c58a47221d69641247f6b2daa73bb119243a27e
Daniel Jimenez Pacheco <daninafria@hotmail.es>
2019-11-06 11:56:34 +0100

    Primera Aproximacion casillas

---
+++ ICE3/Assets/TileImplementation/Scripts/CubeFace.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFace : MonoBehaviour
+{
+    public float width;
+    public float heigth;
+    public int cubeId;
+
+    public GameObject prefabIce;
+    public GameObject prefabRock;
+    public GameObject prefabTile;
+
+    public int FaceNum; //enumerador que sirve para saber que cara es;
+
+    int probRock = 10;
+
+    float prefWidth = 1;
+
+    GameObject[,] cubeFace;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeFace = new GameObject[(int)width, (int)heigth];
+
+        for(int x = 0; x<width; x++)
+        {
+            for (int y = 0; y <heigth; y++)
+            {
+                int random = Random.Range(0, 100);
+                TileScript.type tileType;
+                GameObject tileTypeObject;
+
+                if(random < probRock)
+                {
+                    tileType = TileScript.type.ROCK;
+                    tileTypeObject = prefabRock;
+                } else
+                {
+                    tileType = TileScript.type.ICE;
+                    tileTypeObject = prefabIce;
+                }
+
+                GameObject tileObject = Instantiate(prefabTile,GetComponentInParent<Transform>());
+                tileObject.transform.position = new Vector3(x, 0, y);                
+                TileScript tile = tileObject.GetComponent<TileScript>();
+                    tile.initTile(x, y, cubeId, x, y, tileTypeObject,tileType);
+                cubeFace[x, y] = tileObject;
+            }
+        }
+
+        switch (FaceNum)
+        {
+            case 0: // cara arriba
+                break;
+            case 1: //cara derecha
+                this.transform.Translate(0, -(prefWidth / 2), width - (prefWidth / 2));
+                this.transform.Rotate(90, 0, 0);
+                break;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ICE3/Assets/TileImplementation/Scripts/TileScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+
+    int indexX;
+    int indexY;
+    int cubeId;
+
+    GameObject myObject;
+
+    int posX;
+    int posY;
+
+    public enum type { ICE,ROCK}
+
+    public type tileType;
+
+
+    private void Start()
+    {
+        
+    }
+
+    public void initTile(int indexX, int indexY, int cubeId, int posX, int posY, GameObject prefab, type tipe)
+    {
+        this.indexX = indexX;
+        this.indexY = indexY;
+        this.cubeId = cubeId;
+        myObject = Instantiate(prefab,this.GetComponentInParent<Transform>());
+        tileType = tipe;
+    }
+}
