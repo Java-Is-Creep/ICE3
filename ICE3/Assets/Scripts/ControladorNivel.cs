5c8ec19834e54c64715c7261ba6d472d88e73e7c
Daniel Jimenez Pacheco <daninafria@hotmail.es>
2019-11-23 04:16:57 +0100

    Kit Balas hecho

---
+++ ICE3/Assets/Scripts/ControladorNivel.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ControladorNivel : MonoBehaviourPunCallbacks
+{
+    int TimeToCreateAmmunition = 20;
+    float actualTime = 0;
+    Cube Cubo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Cubo = FindObjectOfType<Cube>();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            if (actualTime < TimeToCreateAmmunition)
+            {
+                actualTime += Time.deltaTime;
+            }
+            else
+            {
+                createAmmunationKit();
+                TimeToCreateAmmunition = Random.Range(15, 26);
+                actualTime = 0;
+            }
+        }
+       
+    }
+
+
+    public void createAmmunationKit()
+    {
+        int indexX;
+        int indexY;
+        int cara;
+        TileScript ts;
+
+        do
+        {
+            indexX = Random.Range(0, (int)Cubo.heigth);
+            indexY = Random.Range(0, (int)Cubo.width);
+            cara = Random.Range(0, 6);
+            ts = Cubo.faces[cara].tiles[indexX, indexY].GetComponent<TileScript>();
+        } while (ts.myObjectType != TileScript.tileObject.NULL);
+
+        GameObject aux = PhotonNetwork.InstantiateSceneObject("KitBalas", ts.AbsolutePos,Quaternion.identity);
+        switch (cara)
+        {
+            case (0):
+
+                break;
+            case (1):
+                aux.transform.Rotate(new Vector3(0, 0, 90));
+                break;
+            case (2):
+                aux.transform.Rotate(new Vector3(0, 0, -90));
+                break;
+            case (3):
+                aux.transform.Rotate(new Vector3(90, 0, 0));
+                break;
+            case (4):
+                aux.transform.Rotate(new Vector3(-90, 0, 0));
+                break;
+            case (5):
+                Debug.Log("Cambiando de cara");
+                aux.transform.Rotate(new Vector3(0, 0, 180));
+                break;
+        }
+        aux.transform.position += aux.transform.TransformDirection(Vector3.up);
+    }
+
+
+}
---
+++ ICE3/Assets/Scripts/KitBalas.cs
@@ -0,0 +1,74 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+
+public class KitBalas : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Choque con algo");
+        if (other.tag == "Player")
+        {
+            Debug.Log("Choque con jugador");
+            this.photonView.RPC("Destroy", RpcTarget.MasterClient);
+        }
+    }
+
+    public void crash()
+    {
+        Debug.Log("Choque con jugador mandado por el");
+        this.photonView.RPC("Destroy", RpcTarget.MasterClient);
+    }
+
+    [PunRPC]
+    void Destroy()
+    {
+        
+        Debug.Log("Destruido por master");
+        PhotonNetwork.Destroy(this.gameObject);
+
+    }
+
+    /*
+    public void init( TileScript ts,int cara)
+    {
+        this.transform.position = ts.AbsolutePos;
+        switch (cara)
+        {
+            case (0):
+                break;
+            case (1):
+                this.transform.Rotate(new Vector3(0, 0, 90));
+                break;
+            case (2):
+                this.transform.Rotate(new Vector3(0, 0, -90));
+                break;
+            case (3):
+                this.transform.Rotate(new Vector3(90, 0, 0));
+                break;
+            case (4):
+                this.transform.Rotate(new Vector3(-90, 0, 0));
+                break;
+            case (5):
+                Debug.Log("Cambiando de cara");
+                this.transform.Rotate(new Vector3(0, 0, 180));
+                break;
+        }
+        this.transform.position += this.transform.TransformDirection(Vector3.up*1.5f);
+    }
+    */
+}
