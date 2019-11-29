553de280d4f3b3b524f7a5383a1cc36e8014cfed
Axel <axelsax1998@gmail.com>
2019-11-29 03:31:28 +0100

    Necesito el controller jajan't

---
+++ ICE3/Assets/Scripts/moverCamaraFijaMenu.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.SceneManagement;
+
+public class moverCamaraFijaMenu : MonoBehaviour
+{
+
+    Animator anim;
+    int cara;
+    int lastCara;
+    public CinemachineVirtualCamera topCam;
+    public CinemachineVirtualCamera buttonCam;
+    public CinemachineVirtualCamera frontCam;
+    public CinemachineVirtualCamera backCam;
+    public CinemachineVirtualCamera leftCam;
+    public CinemachineVirtualCamera rightCam;
+
+    public Canvas cvTop;
+    public Canvas cvFront;
+    public Canvas cvBottom;
+    public Canvas cvLeft;
+    public Canvas cvRight;
+    public Canvas cvBack;
+
+    private Canvas activedCanvas;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+        cara = 0;
+        activedCanvas = cvFront;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            Debug.Log("0");
+            top();
+        }
+        else if(Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            Debug.Log("1");
+            left();
+        } else if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            Debug.Log("2");
+            button();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            Debug.Log("3");
+            right();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            Debug.Log("4");
+            front();
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            Debug.Log("5");
+            back();
+        } 
+    }
+
+    public void front()
+    {
+        Debug.Log("Front");
+        activedCanvas.gameObject.SetActive(false);
+        cvFront.gameObject.SetActive(true);
+        activedCanvas= cvFront;
+        cara = 4;
+        anim.SetInteger("cara", 4);
+    }
+
+    public void back()
+    {
+        activedCanvas.gameObject.SetActive(false);
+        cvBack.gameObject.SetActive(true);
+        activedCanvas = cvBack;
+        cara = 5;
+        anim.SetInteger("cara", 5);
+    }
+
+    public void top()
+    {
+        Debug.Log("Top");
+        activedCanvas.gameObject.SetActive(false);
+        cvTop.gameObject.SetActive(true);
+        activedCanvas = cvTop;
+        cara = 0;
+        anim.SetInteger("cara", 0);
+    }
+
+    public void button()
+    {
+        activedCanvas.gameObject.SetActive(false); ;
+        cvBottom.gameObject.SetActive(true);
+        activedCanvas = cvBottom;
+        cara = 2;
+        anim.SetInteger("cara", 2);
+    }
+
+    public void left()
+    {
+        activedCanvas.gameObject.SetActive(false);
+        cvLeft.gameObject.SetActive(true);
+        activedCanvas = cvLeft;
+        cara = 1;
+        anim.SetInteger("cara", 1);
+    }
+
+    public void right()
+    {
+        activedCanvas.gameObject.SetActive(false);
+        cvRight.gameObject.SetActive(true);
+        activedCanvas = cvRight;
+        cara = 3;
+        anim.SetInteger("cara", 3);
+    }
+
+    public void jugar()
+    {
+        SceneManager.LoadScene("Launcher");
+    }
+
+}
