eadaa8e9156b5dabb4b457cdd534648b1fb25f5f
heyimsergio <51760253+heyimsergio@users.noreply.github.com>
2019-12-02 04:42:50 +0100

    metiendo sonidos

--- ICE3/Assets/Scripts/InitGame.cs
+++ ICE3/Assets/Scripts/InitGame.cs
@@ -5,8 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class InitGame : MonoBehaviour
 {
+
+    public AudioSource a;
+
     public void onClick()
     {
+        a.Play();
         SceneManager.LoadScene("InitSesion");
     }
 }
