4c23af146ec1511c80271da11293d5416d580350
Axel <axelsax1998@gmail.com>
2019-12-02 03:39:11 +0100

    Mas y mas y mas interfaces, ahora hay menu e inicio sesion

---
+++ ICE3/Assets/Scripts/InitSesion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InitSesion : MonoBehaviour
+{
+    [SerializeField]
+    private InputField playerName;
+    [SerializeField]
+    private InputField playerNameMobile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Application.isMobilePlatform)
+        {
+            // Crear
+            playerNameMobile.gameObject.SetActive(true);
+
+        }
+        else
+        {
+            Debug.Log("no movil");
+            // crear
+            playerName.gameObject.SetActive(true);
+        }
+    }
+
+    public void onClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
