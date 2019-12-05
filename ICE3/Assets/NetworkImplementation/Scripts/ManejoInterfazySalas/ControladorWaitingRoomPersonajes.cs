fdd9694c49c81c4176bc81133419206b5d83eca2
Daniel Jimenez Pacheco <daninafria@hotmail.es>
2019-12-06 00:58:53 +0100

    Cambios Update

--- ICE3/Assets/NetworkImplementation/Scripts/ManejoInterfazySalas/ControladorWaitingRoomPersonajes.cs
+++ ICE3/Assets/NetworkImplementation/Scripts/ManejoInterfazySalas/ControladorWaitingRoomPersonajes.cs
@@ -10,6 +10,9 @@ public class ControladorWaitingRoomPersonajes : MonoBehaviourPunCallbacks
     Player[] jugadores;
     [SerializeField]
     private Text texto;
+    private bool refrescar;
+    public float tiempoParaRecargar;
+    public float actualTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,20 +20,32 @@ public class ControladorWaitingRoomPersonajes : MonoBehaviourPunCallbacks
         ExitGames.Client.Photon.Hashtable character = new ExitGames.Client.Photon.Hashtable();
         character.Add("skin", PlayerPrefs.GetInt("IndiceEscenario"));
         PhotonNetwork.LocalPlayer.SetCustomProperties(character);
+        refrescar = false;
+        tiempoParaRecargar = 0.5f;
+        actualTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (refrescar)
+        {
+            actualTime += Time.deltaTime;
+            if(actualTime > tiempoParaRecargar)
+            {
+                refrescar = false;
+                actualTime = 0;
+                refrescarNombres();
+            }
+        }
     }
 
-    public override void OnPlayerLeftRoom(Player otherPlayer)
+    public void refrescarNombres()
     {
         string nombres = " Tu eleccion es: " + PlayerPrefs.GetInt("IndiceEscenario");
-        Debug.Log("ALGUIEN SE FUE");
         jugadores = PhotonNetwork.PlayerList;
-        foreach(Player play in jugadores)
+        foreach (Player play in jugadores)
         {
             object indice;
             play.CustomProperties.TryGetValue("skin", out indice);
@@ -45,25 +60,21 @@ public class ControladorWaitingRoomPersonajes : MonoBehaviourPunCallbacks
         texto.text = nombres;
     }
 
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+
+        Debug.Log("ALGUIEN SE FUE");
+        refrescar = true;
+    }
+
+
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
-        string nombres = " Tu eleccion es: " + PlayerPrefs.GetInt("IndiceEscenario");
+
         Debug.Log("ALGUIEN ENTRA");
-        jugadores = PhotonNetwork.PlayerList;
-        foreach (Player play in jugadores)
-        {
-            object indice;
-            play.CustomProperties.TryGetValue("skin", out indice);
-            if(indice != null)
-            {
-                int index = (int)indice;
-                Debug.Log("Jugador: " + play.NickName + " lleva la skin de: " + index);
-                nombres.Insert(nombres.Length, "Jugador: " + play.NickName + " lleva la skin de: " + index + "\n");
-            }
-
-            texto.text = nombres;
-
-        }
+        refrescar = true;
+      
     }
 
 
