597326778e9d804892d2f8b6cd7d972ffceb44e9
Daniel Jimenez Pacheco <daninafria@hotmail.es>
2019-12-06 00:05:13 +0100

    Prueba1

---
+++ ICE3/Assets/NetworkImplementation/Scripts/ManejoInterfazySalas/ControladorWaitingRoomPersonajes.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ControladorWaitingRoomPersonajes : MonoBehaviourPunCallbacks 
+{
+    Player[] jugadores;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jugadores = PhotonNetwork.PlayerList;
+        ExitGames.Client.Photon.Hashtable character = new ExitGames.Client.Photon.Hashtable();
+        character.Add("skin", PlayerPrefs.GetInt("IndiceEscenario"));
+        PhotonNetwork.LocalPlayer.SetCustomProperties(character);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.Log("ALGUIEN SE FUE");
+        jugadores = PhotonNetwork.PlayerList;
+        foreach(Player play in jugadores)
+        {
+            object indice;
+            play.CustomProperties.TryGetValue("skin", out indice);
+            if (indice != null)
+            {
+                int index = (int)indice;
+                Debug.Log("Jugador: " + play.NickName + " lleva la skin de: " + index);
+            }
+
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("ALGUIEN ENTRA");
+        jugadores = PhotonNetwork.PlayerList;
+        foreach (Player play in jugadores)
+        {
+            object indice;
+            play.CustomProperties.TryGetValue("skin", out indice);
+            if(indice != null)
+            {
+                int index = (int)indice;
+                Debug.Log("Jugador: " + play.NickName + " lleva la skin de: " + index);
+            }
+
+        }
+    }
+
+
+
+
+
+
+}
