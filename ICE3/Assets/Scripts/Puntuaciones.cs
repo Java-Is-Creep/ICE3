d338b103735812ff6391a5250c1c17d6594f8caf
Daniel Jimenez Pacheco <daninafria@hotmail.es>
2019-12-06 22:26:58 +0100

    ya va

--- ICE3/Assets/Scripts/Puntuaciones.cs
+++ ICE3/Assets/Scripts/Puntuaciones.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.UI;
 
 public class Puntuaciones : MonoBehaviourPunCallbacks
 {
 
     public Dictionary<int, Puntuacion> puntuaciones;
+    [SerializeField]
+    Text textoPuntuaciones;
 
     // Start is called before the first frame update
     void Start()
@@ -40,6 +43,32 @@ public class Puntuaciones : MonoBehaviourPunCallbacks
             Puntuacion aux2 = new Puntuacion(play.NickName);
             puntuaciones.Add(id,aux2);
         }
+
+
+        List<Puntuacion> listaOrdenada = new List<Puntuacion>();
+
+
+        string puntuacionesString = "";
+        foreach (int key in puntuaciones.Keys)
+        {
+
+            Puntuacion aux3;
+            puntuaciones.TryGetValue(key, out aux3);
+            Debug.Log("La puntuacion de: " + aux3.nombre + " es: " + aux3.puntuacion + "\n");
+            listaOrdenada.Add(aux3);
+        }
+
+        listaOrdenada.Sort(new ComparadorPuntuacion());
+
+        foreach (Puntuacion punt in listaOrdenada)
+        {
+            Debug.Log("La puntuacion de: " + punt.nombre + " es: " + punt.puntuacion);
+            puntuacionesString += "La puntuacion de: " + punt.nombre + " es: " + punt.puntuacion + "\n";
+            textoPuntuaciones.text = puntuacionesString;
+        }
+
+
+
     }
 
 }
